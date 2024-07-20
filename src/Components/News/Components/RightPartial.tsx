40b58e0ac5c8b3657d5b87919873c04c4a59913c
TSM97 <Gtsamadias@hotmail.com>
2024-07-20 21:09:47 +0300

    Implement(Dynamic Data in News Section)

--- src/Components/News/Components/RightPartial.tsx
+++ src/Components/News/Components/RightPartial.tsx
@@ -1,60 +1,35 @@
-import News1 from "../../../assets/News1.webp";
-import News2 from "../../../assets/News2.webp";
+import { articleData } from "../data";
 
 export default function RightPartial() {
   return (
-    <article className="text-left h-full p-4 max-w-[40vw]">
+    <article className="text-left h-full p-4 max-w-[80dvw] md:max-w-[40dvw]">
       <header className="font-bold text-3xl pb-4">News</header>
-      <div>
-        <div className="p-6 flex border-gray-800 rounded-lg shadow-md mb-4">
-          <img
-            className="object-cover w-1/2 mr-4"
-            alt="article image"
-            style={{ borderRadius: "30% 70% 41% 59% / 74% 14% 86% 26%" }}
-            src={News1}
-          />
-          <section>
-            <a href="#">
-              <h5 className="mb-2 font-bold tracking-tight">
-                Lorem ipsum dolor sit amet consectetur.
-              </h5>
-            </a>
-            <p className="mb-3 font-normal">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Voluptate explicabo nisi ipsam!
-            </p>
-            <div className="flex w-full justify-end">
-              <button className="rounded-2xl border-2 border-dashed border-black bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_#e8772e] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
-                Read more
-              </button>
-            </div>
-          </section>
-        </div>
-        <div className="p-6 flex border-gray-800 rounded-lg shadow-md mb-4">
-          <img
-            className="object-cover w-1/2 mr-4"
-            alt="article image"
-            style={{ borderRadius: "30% 70% 41% 59% / 74% 14% 86% 26%" }}
-            src={News2}
-          />
-          <section>
-            <a href="#">
-              <h5 className="mb-2 font-bold tracking-tight">
-                Lorem ipsum dolor sit amet consectetur adipisicing.
-              </h5>
-            </a>
-            <p className="mb-3 font-normal">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-              aut corporis ratione quod. Quas, neque?
-            </p>
-            <div className="flex w-full justify-end">
-              <button className="rounded-2xl border-2 border-dashed border-black bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_#e8772e] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
-                Read more
-              </button>
-            </div>
-          </section>
-        </div>
-      </div>
+      {articleData.map((article) => {
+        return (
+          <div className="p-6 flex flex-col lg:flex-row gap-3 border-gray-800 rounded-lg shadow-md mb-4">
+            <img
+              className="object-cover lg:w-1/2 mr-4 none"
+              alt="article image"
+              style={{ borderRadius: "30% 70% 41% 59% / 74% 14% 86% 26%" }}
+              src={article.Image}
+            />
+            <section className="flex flex-col justify-between">
+              <a href="#">
+                <h5 className="mb-2 font-bold text-xl tracking-tight">
+                  {article.Title}
+                </h5>
+              </a>
+              <p className="mb-3 font-normal">{article.Description}</p>
+              <div className="flex w-full justify-end">
+                <button className="rounded-2xl border-2 border-dashed border-black bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_#e8772e] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
+                  Read more
+                </button>
+              </div>
+              <div className="text-end align-bottom pt-2">{article.Date}</div>
+            </section>
+          </div>
+        );
+      })}
     </article>
   );
 }
