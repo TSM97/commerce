7cdb0156ff05dc3f119a9c04a49c51b123c20912
TSM97 <Gtsamadias@hotmail.com>
2024-07-28 00:31:24 +0300

    Implement(articleDelete modal) - Need to Implement firestore deletion

--- src/AdminPage/Admin.tsx
+++ src/AdminPage/Admin.tsx
@@ -1,13 +1,16 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { db } from "../firebaseConfig";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
 
 import uploadImage from "../Utils/uploadImage";
-import { db } from "../firebaseConfig";
-import { addDoc, collection } from "firebase/firestore";
+import AdminModal from "./Components/AdminModal";
 
 export default function Admin() {
   const titleRef = useRef<HTMLInputElement>(null);
   const shortDescRef = useRef<HTMLInputElement>(null);
   const fullDescRef = useRef<HTMLTextAreaElement>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleOnSubmit = async (e: any) => {
     e.preventDefault();
@@ -21,7 +24,7 @@ export default function Admin() {
         title,
         shortDescription,
         fullDescription,
-        createdAt: new Date(),
+        createdAt: Timestamp.now(),
       });
       console.log("Document written with ID: ", docRef.id);
 
@@ -40,73 +43,86 @@ export default function Admin() {
   // uploadImage("dropzone-file");
 
   return (
-    <section className="container m-auto min-h-screen pt-10">
-      <div className="text-4xl my-8 pb-2 text-center border-2 border-t-0 border-r-0 border-l-0 border-b-black border-dashed">
-        Article Upload
-      </div>
-      <form onSubmit={handleOnSubmit}>
-        <div className="grid gap-6 mb-6 md:grid-cols-2">
-          <div>
-            <label
-              htmlFor="Title"
-              className="block mb-2 text-lg font-medium text-white-900"
-            >
-              Title
-            </label>
-            <input
-              ref={titleRef}
-              type="text"
-              id="Title"
-              className="bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white"
-              placeholder="John"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="shortDesc"
-              className="block mb-2 text-lg font-medium text-white-900"
-            >
-              Short Description
-            </label>
-            <input
-              ref={shortDescRef}
-              type="text"
-              id="shortDesc"
-              className="bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white"
-              placeholder="Doe"
-              required
-            />
-          </div>
-          <section className="col-span-2">
-            <label
-              htmlFor="fullDesc"
-              className="block mb-2 text-lg font-medium"
-            >
-              Full Description
-            </label>
-            <textarea
-              ref={fullDescRef}
-              id="fullDesc"
-              rows={4}
-              className="block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
-              placeholder="Write your article`s Section"
-            />
-          </section>
-          {/* TODO: NOT READY YET - Should Implement React-Drop-zone for this element 
+    <>
+      <section className="container m-auto min-h-screen pt-10">
+        <div className="text-4xl my-8 pb-2 text-center border-2 border-t-0 border-r-0 border-l-0 border-b-black border-dashed">
+          Article Upload
+        </div>
+        <form onSubmit={handleOnSubmit}>
+          <div className="grid gap-6 mb-6 md:grid-cols-2">
+            <div>
+              <label
+                htmlFor="Title"
+                className="block mb-2 text-lg font-medium text-white-900"
+              >
+                Title
+              </label>
+              <input
+                ref={titleRef}
+                type="text"
+                id="Title"
+                className="bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white"
+                placeholder="John"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="shortDesc"
+                className="block mb-2 text-lg font-medium text-white-900"
+              >
+                Short Description
+              </label>
+              <input
+                ref={shortDescRef}
+                type="text"
+                id="shortDesc"
+                className="bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white"
+                placeholder="Doe"
+                required
+              />
+            </div>
+            <section className="col-span-2">
+              <label
+                htmlFor="fullDesc"
+                className="block mb-2 text-lg font-medium"
+              >
+                Full Description
+              </label>
+              <textarea
+                ref={fullDescRef}
+                id="fullDesc"
+                rows={4}
+                className="block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+                placeholder="Write your article`s Section"
+              />
+            </section>
+            {/* TODO: NOT READY YET - Should Implement React-Drop-zone for this element 
 
           {/* <section className="col-span-2">
             <ImgUpload />
           </section> */}
-        </div>
-
-        <button
-          type="submit"
-          className="text-black= bg-primary hover:bg-primary-500 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center"
-        >
-          Submit
-        </button>
-      </form>
-    </section>
+          </div>
+          <div className="flex gap-4 justify-between">
+            <button
+              type="submit"
+              className=" bg-primary hover:bg-primary-500 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              Submit
+            </button>
+            <button
+              onClick={() => setIsOpen(true)}
+              data-modal-target="AdminModal"
+              data-modal-toggle="AdminModal"
+              type="button"
+              className="border-black border-2 hover:bg-red-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              Delete the last article that you Uploaded
+            </button>
+          </div>
+        </form>
+      </section>
+      <AdminModal isOpen={isOpen} setIsOpen={setIsOpen} />
+    </>
   );
 }
--- src/Components/News/Components/RightPartial.tsx
+++ src/Components/News/Components/RightPartial.tsx
@@ -1,10 +1,54 @@
 import { articleData } from "../data";
+import { collection, getDocs, Timestamp } from "firebase/firestore";
+import { db } from "../../../firebaseConfig"; // Adjust the path to your firebaseConfig file
+import { useEffect, useState } from "react";
+
+import Image1 from "../../../assets/News1.webp";
+
+type Article = {
+  id: string;
+  title: string;
+  shortDescription: string;
+  fullDescription: string;
+  createdAt: Timestamp;
+};
 
 export default function RightPartial() {
+  const [articles, setArticles] = useState<Article[]>([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "articles"));
+        const articlesList: Article[] = [];
+        querySnapshot.forEach((doc) => {
+          articlesList.push({
+            id: doc.id,
+            ...(doc.data() as Omit<Article, "id">),
+          });
+        });
+        setArticles(articlesList);
+      } catch (error) {
+        console.error("Error fetching articles: ", error);
+      }
+    };
+    fetchArticles();
+  }, []);
+
   return (
     <article className="text-left h-full p-4 max-w-[80dvw] md:max-w-[40dvw]">
       <header className="font-bold text-3xl pb-4">News</header>
-      {articleData.map((article, key) => {
+      {articles.map((article, key) => {
+        // const date = new Intl.DateTimeFormat("en-US", {
+        //   year: "numeric",
+        //   month: "2-digit",
+        //   day: "2-digit",
+        // }).format(article?.createdAt.toDate());
+        const formattedDate = new Intl.DateTimeFormat("en-US", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+        }).format(article?.createdAt?.toDate());
         return (
           <div
             key={`${article}${key}`}
@@ -14,21 +58,21 @@ export default function RightPartial() {
               className="object-cover lg:w-1/2 mr-4 none"
               alt="article image"
               style={{ borderRadius: "30% 70% 41% 59% / 74% 14% 86% 26%" }}
-              src={article.Image}
+              src={Image1}
             />
-            <section className="flex flex-col justify-between">
+            <section className="flex flex-col justify-between w-full">
               <a href="#">
                 <h5 className="mb-2 font-bold text-xl tracking-tight">
-                  {article.Title}
+                  {article?.title}
                 </h5>
               </a>
-              <p className="mb-3 font-normal">{article.Description}</p>
+              <p className="mb-3 font-normal">{article?.shortDescription}</p>
               <div className="flex w-full justify-end">
                 <button className="rounded-2xl border-2 border-dashed border-black bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_#e8772e] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
                   Read more
                 </button>
               </div>
-              <div className="text-end align-bottom pt-2">{article.Date}</div>
+              <div className="text-end align-bottom pt-2">{formattedDate}</div>
             </section>
           </div>
         );
