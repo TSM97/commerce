ab0e1188367779fa8c832fd52930f967b8eaa0a8
TSM97 <Gtsamadias@hotmail.com>
2024-09-05 21:30:42 +0300

    fix(Order on Products-Mobile)

--- src/Components/Products/Components/DraggableProducts.tsx
+++ src/Components/Products/Components/DraggableProducts.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import { motion, useMotionValue } from 'framer-motion';
+import { useEffect, useState } from "react";
+import { motion, useMotionValue } from "framer-motion";
 
-import { products, includesOneProduct, SPRING_OPTIONS } from '../data';
-import Dots from './Dots';
-import CustomButton from '../../CustomButton';
+import { products, includesOneProduct, SPRING_OPTIONS } from "../data";
+import Dots from "./Dots";
+import CustomButton from "../../CustomButton";
 
 const ONE_SECOND = 1000;
 const AUTO_DELAY = ONE_SECOND * 10;
-const DRAG_BUFFER = 50;
+const DRAG_BUFFER = 10;
 
 export const DraggableProducts = () => {
   const [imgIndex, setImgIndex] = useState(0);
@@ -42,10 +42,13 @@ export const DraggableProducts = () => {
   };
 
   return (
-    <section className='w-full lg:min-w-[50%] h-full flex flex-col items-center justify-center lg:w-1/2 py-[2dvw]'>
-      <div className='w-3/4 md:w-[65%] h-full relative overflow-hidden pb-8'>
+    <section
+      id="Products"
+      className="w-full lg:min-w-[50%] h-full flex flex-col items-center justify-center lg:w-1/2 py-[2dvw]  lg:order-1 order-2"
+    >
+      <div className="w-3/4 md:w-[65%] h-full relative overflow-hidden pb-8">
         <motion.div
-          drag={includesOneProduct ? false : 'x'}
+          drag={includesOneProduct ? false : "x"}
           dragConstraints={{
             left: 0,
             right: 0,
@@ -60,42 +63,42 @@ export const DraggableProducts = () => {
           transition={SPRING_OPTIONS}
           onDragEnd={onDragEnd}
           className={`flex ${
-            !includesOneProduct && 'cursor-grab active:cursor-grabbing'
+            !includesOneProduct && "cursor-grab active:cursor-grabbing"
           } h-[90%] w-full items-center`}
         >
           {products.map((product, idx) => {
             return (
               <div
                 key={idx}
-                className='min-w-[97%] relative lg:h-full mx-[1.5%] bg-white shadow-md rounded-3xl duration-500 hover:shadow-lg'
+                className="min-w-[97%] relative lg:h-full mx-[1.5%] bg-white shadow-md rounded-3xl duration-500 hover:shadow-lg"
               >
                 <img
-                  draggable='false'
-                  className='aspect-square object-cover w-full h-1/2 lg:h-3/4 rounded-t-xl bg-neutral-800'
+                  draggable="false"
+                  className="aspect-square object-cover w-full h-1/2 lg:h-3/4 rounded-t-xl bg-neutral-800"
                   src={product?.img}
                 />
-                <div className='px-4 py-3 full'>
-                  <span className='text-gray-400 mr-3 uppercase text-xs'>
+                <div className="px-4 py-3 full">
+                  <span className="text-gray-400 mr-3 uppercase text-xs">
                     {product?.subtitle}
                   </span>
-                  <p className='text-lg font-bold text-black capitalize'>
+                  <p className="text-lg font-bold text-black capitalize">
                     {product?.title}
                   </p>
-                  <div className='flex items-center'>
-                    <p className='text-lg font-semibold text-black cursor-auto my-3'>
+                  <div className="flex items-center">
+                    <p className="text-lg font-semibold text-black cursor-auto my-3">
                       {product?.lastPrice}
                     </p>
                     <del>
-                      <p className='text-sm text-gray-600 cursor-auto ml-2'>
+                      <p className="text-sm text-gray-600 cursor-auto ml-2">
                         {product?.firstPrice}
                       </p>
                     </del>
                   </div>
                 </div>
                 <CustomButton
-                  elementType='navLink'
-                  className='absolute bottom-3 right-3'
-                  to={`/#Home`}
+                  elementType="navLink"
+                  className="absolute bottom-3 right-3"
+                  to={`/#Contact`}
                 >
                   Contact
                 </CustomButton>
