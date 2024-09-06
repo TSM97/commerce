54400e57f42b51a83d0b85445e84270001fb0be7
TSM97 <Gtsamadias@hotmail.com>
2024-09-06 14:17:42 +0300

    small-fix(image in articles width fix)

--- src/Components/News/Components/Articles.tsx
+++ src/Components/News/Components/Articles.tsx
@@ -1,6 +1,6 @@
-import { useNavigate } from 'react-router-dom';
-import ArticleType from '../../../types/articleType';
-import CustomButton from '../../CustomButton';
+import { useNavigate } from "react-router-dom";
+import ArticleType from "../../../types/articleType";
+import CustomButton from "../../CustomButton";
 
 export default function Articles({ articles }: { articles: ArticleType[] }) {
   const navigate = useNavigate();
@@ -12,51 +12,51 @@ export default function Articles({ articles }: { articles: ArticleType[] }) {
     <>
       {articles.map((article) => {
         //format date in order to show to NewsMini
-        const formattedDate = new Intl.DateTimeFormat('en-US', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
+        const formattedDate = new Intl.DateTimeFormat("en-US", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
           hour12: false,
         }).format(article?.createdAt?.toDate());
 
         return (
           <article
             key={article?.id}
-            className='p-6 flex flex-col lg:flex-row gap-3 bg-white border-gray-800 rounded-lg shadow-md mb-4 lg:h-[300px]'
+            className="p-6 flex flex-col lg:flex-row gap-3 bg-white border-gray-800 rounded-lg shadow-md mb-4 lg:h-[300px]"
           >
             <img
-              className='object-cover w-full h-[150px] lg:h-[250px] sm:w-1/2 mr-4 none'
-              loading='lazy'
+              className="object-cover sm:w-1/2 h-[150px] lg:h-[250px] mr-4 none"
+              loading="lazy"
               alt="Article's image"
-              style={{ borderRadius: '30% 70% 41% 59% / 74% 14% 86% 26%' }}
+              style={{ borderRadius: "30% 70% 41% 59% / 74% 14% 86% 26%" }}
               src={article?.imageUrl}
             />
-            <section className='flex flex-col justify-between w-full overflow-y-hidden'>
+            <section className="flex flex-col justify-between w-full overflow-y-hidden">
               <h5
-                id='titleID'
-                className='mb-2 font-bold md:text-2xl tracking-tight overflow-y-hidden text-base line-clamp-3 md:line-clamp-2'
+                id="titleID"
+                className="mb-2 font-bold md:text-2xl tracking-tight overflow-y-hidden text-base line-clamp-3 md:line-clamp-2"
               >
                 {article?.title}
               </h5>
               <p
-                id='short_DescriptionId'
-                className='mb-3 md:text-xl font-normal overflow-y-hidden  text-base line-clamp-6 md:line-clamp-3'
+                id="short_DescriptionId"
+                className="mb-3 md:text-xl font-normal overflow-y-hidden  text-base line-clamp-6 md:line-clamp-3"
               >
                 {article?.shortDescription}
               </p>
-              <div className='flex w-full items-center md:items-end justify-end md:flex-col flex-row gap-2'>
-                {article.aTag !== '' ? (
+              <div className="flex w-full items-center md:items-end justify-end md:flex-col flex-row gap-2">
+                {article.aTag !== "" ? (
                   <CustomButton
-                    elementType='anchorTag'
-                    target='_blank'
+                    elementType="anchorTag"
+                    target="_blank"
                     href={article.aTag}
                     ariaLabel={`Read more about ${article.title}`}
-                    className='rounded-2xl cursor-pointer border-2 border-dashed border-black bg-white px-2 md:px-6 py-1 md:py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_#e8772e] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none'
+                    className="rounded-2xl cursor-pointer border-2 border-dashed border-black bg-white px-2 md:px-6 py-1 md:py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_#e8772e] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none"
                   >
                     Read more
-                    <span className='sr-only'>
+                    <span className="sr-only">
                       Read this article about {article.title}
                     </span>
                   </CustomButton>
@@ -65,7 +65,7 @@ export default function Articles({ articles }: { articles: ArticleType[] }) {
                     Read more
                   </CustomButton>
                 )}
-                <div className='text-end align-bottom md:pt-2'>
+                <div className="text-end align-bottom md:pt-2">
                   {formattedDate}
                 </div>
               </div>
