ec3ea54b597ff470959838ba98e16b59b85a9b33
GiannisTsamadias <127104855+GiannisTsamadias@users.noreply.github.com>
2024-09-18 16:38:59 +0300

    Implement(UploadData as reusable util)

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,38 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 // import Lenis from 'lenis';
 
-import NavBar from "./Components/NavBar";
-import Admin from "./AdminPage/Admin";
-import { Hero, Intro } from "./Components/Hero";
-import News from "./Components/News/News";
-import { ArticlePage } from "./Components/ArticlePage";
-import Products from "./Components/Products";
-import About from "./Components/About";
+import NavBar from './Components/NavBar';
+import Admin from './AdminPage/Admin';
+import { Hero, Intro } from './Components/Hero';
+import News from './Components/News/News';
+import { ArticlePage } from './Components/ArticlePage';
+import Products from './Components/Products';
+import About from './Components/About';
 
-import "./App.css";
-import { useArticlesStore } from "./stores/useArticlesStore";
+import './App.css';
+import { useArticlesStore } from './stores/useArticlesStore';
 
 export default function App() {
   const location = useLocation(); // Get the current location
 
+  //! If I upload an article data to the users dont refresh. Is it actually a problem at our case?
   const { fetchArticles } = useArticlesStore();
 
   useEffect(() => {
     fetchArticles(); // Fetch articles on component mount
   }, [fetchArticles]);
 
-  const isAdminPage = location.pathname === "/admin";
+  const isAdminPage = location.pathname === '/admin';
 
   return (
-    <section id="main">
-      <section className="flex justify-center w-100dvw">
+    <section id='main'>
+      <section className='flex justify-center w-100dvw'>
         {!isAdminPage && <NavBar />}
       </section>
       <Routes>
         <Route
-          path="/"
+          path='/'
           element={
             <>
               <Intro />
@@ -39,12 +40,12 @@ export default function App() {
               <Hero />
               <News />
               <About />
-              <section className="h-screen bg-white z-10"></section>
+              <section className='h-screen bg-white z-10'></section>
             </>
           }
         />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/article/:id" element={<ArticlePage />} />
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/article/:id' element={<ArticlePage />} />
       </Routes>
     </section>
   );
---
+++ src/Utils/uploadDataToFireStore.ts
@@ -0,0 +1,27 @@
+import { db } from '../firebaseConfig';
+import { addDoc, collection, Timestamp } from 'firebase/firestore';
+
+interface UploadDataParams {
+  collectionName: string;
+  imageUrl?: string;
+  fields: { [key: string]: any };
+}
+
+const uploadDataToFirestore = async ({
+  collectionName,
+  imageUrl,
+  fields,
+}: UploadDataParams) => {
+  const data: any = {
+    ...fields,
+    createdAt: Timestamp.now(),
+  };
+
+  if (imageUrl) {
+    data.imageUrl = imageUrl;
+  }
+
+  await addDoc(collection(db, collectionName), data);
+};
+
+export default uploadDataToFirestore;
