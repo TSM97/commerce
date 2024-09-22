666679d173e119d51d4952cbc61f09a151121f1b
GiannisTsamadias <127104855+GiannisTsamadias@users.noreply.github.com>
2024-09-22 18:26:05 +0300

    Add(Hoverable flip-card)-idea for about section

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,7 +1,18 @@
 export default function About() {
   return (
     <section id='About' className='bg-svgColor relative min-h-screen z-10'>
-      <div className='container'></div>
+      <div className='container'>
+        <div className='card w-1/3 h-[600px]'>
+          <div className='card__content text-center relative p-20 transition-transform duration-1000 text-white font-bold'>
+            <div className='card__front absolute top-0 bottom-0 rounded-xl right-0 left-0 p-8 bg-pink-600 flex items-center justify-center h-[400px]'>
+              <h2>Front</h2>
+            </div>
+            <div className='card__back absolute top-0 bottom-0 rounded-xl right-0 left-0 p-8 bg-teal-500 flex items-center justify-center h-[400px]'>
+              <h2>Back</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
