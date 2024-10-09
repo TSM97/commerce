b86dc74701c61fc44094e414708e49f2bbb5527d
GiannisTsamadias <127104855+GiannisTsamadias@users.noreply.github.com>
2024-10-09 18:57:23 +0300

    Add(New About section design)

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,21 +1,32 @@
 export default function About() {
   return (
-    <section id='About' className='bg-svgColor relative min-h-screen z-10'>
-      <div className='container'>
-        {/* <div className='card w-1/3 h-[600px]'>
-          <div className='card__content text-center relative p-20 transition-transform duration-1000 text-white font-bold'>
-            <div className='card__front absolute top-0 bottom-0 rounded-xl right-0 left-0 p-8 bg-pink-600 flex items-center justify-center h-[400px]'>
-              <h2>Front</h2>
-            </div>
-            <div className='card__back absolute top-0 bottom-0 rounded-xl right-0 left-0 p-8 bg-teal-500 flex items-center justify-center h-[400px]'>
-              <h2>Back</h2>
-            </div>
+    <section>
+      <section
+        id='About'
+        className='bg-svgColor relative h-[100dvh] z-10 flex justify-center items-center'
+      >
+        <div className='mx-auto w-1/2 text-center text-9xl text-black-250 tracking-tighter py-14'>
+          Reveal the Unique Story Behind
+          <span className='text-honey'> Atheenian Bees</span>
+        </div>
+      </section>
+
+      <div className='relative h-[calc(100vh+80dvh)] -top-[80vh] bg-red-500'>
+        <div className='h-[80dvh] sticky top-[calc(100vh-80dvh)]'>
+          <div className='w-3/4 text-2xl mx-auto grid grid-cols-2  justify-between items-end'>
+            <div className=''>Sticky Footer</div>
+            <div className=''>©copyright</div>
           </div>
-        </div> */}
-        <div className='w-50'>
-          <h1 className='text-6xl text-black-250 tracking-tighter'>About</h1>
         </div>
       </div>
+
+      {/* <div className='fixed bottom-0 h-[80vh] w-full z-0'>
+        <div className='flex justify-between items-end'>
+          <h1 className='text-[14vw] leading-[0.8] mt-10'>Sticky Footer</h1>
+
+          <p>©copyright</p>
+        </div>
+      </div> */}
     </section>
   );
 }
