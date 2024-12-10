316996937232bee919e7fda0d641235064437a53
GiannisTsamadias <127104855+GiannisTsamadias@users.noreply.github.com>
2024-12-10 17:41:57 +0200

    Addd(some logic in articleUpload in order to split language on upload)

--- src/AdminPage/ArticlesUploadForm.tsx
+++ src/AdminPage/ArticlesUploadForm.tsx
@@ -1,7 +1,9 @@
 import { Timestamp } from 'firebase/firestore';
-import uploadDataToFirestore from '../Utils/uploadDataToFireStore';
+import { motion } from 'framer-motion';
 import { useDropzone } from 'react-dropzone';
 import { useRef, useState } from 'react';
+
+import uploadDataToFirestore from '../Utils/uploadDataToFireStore';
 import UseImageUpload from '../hooks/useImageUpload';
 import AdminModal from './Components/AdminModal';
 import useResetFormFields from './hooks/useResetFormFields';
@@ -13,8 +15,9 @@ export default function ArticlesUploadForm() {
   const fullDescRef = useRef<HTMLTextAreaElement>(null);
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const [isFullDescRequired, setIsFullDescRequired] = useState(true);
-  const { uploadImage, loading, error } = UseImageUpload();
   const [isOpen, setIsOpen] = useState(false);
+  const [greekFormShow, setGreekFormShow] = useState(false);
+  const { uploadImage, loading, error } = UseImageUpload();
   const { resetFormFields } = useResetFormFields(
     { titleRef, shortDescRef, fullDescRef, aTagRef },
     setSelectedImage
@@ -75,78 +78,153 @@ export default function ArticlesUploadForm() {
 
   return (
     <>
-      {' '}
       <form onSubmit={handleOnSubmit}>
         <div className='flex flex-col lg:grid gap-6 mb-6 md:grid-cols-2'>
-          <div>
-            <label
-              htmlFor='Title'
-              className='block mb-2 text-lg font-medium text-white-900'
-            >
-              Title
-            </label>
-            <input
-              ref={titleRef}
-              type='text'
-              id='Title'
-              className='bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white'
-              placeholder='John'
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor='shortDesc'
-              className='block mb-2 text-lg font-medium text-white-900'
-            >
-              Short Description
-            </label>
-            <input
-              ref={shortDescRef}
-              type='text'
-              id='shortDesc'
-              className='bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white'
-              placeholder='Doe'
-              required
-            />
-          </div>
-          <section className='col-span-2'>
-            <div className='lg:flex gap-4'>
-              <label
-                htmlFor='fullDesc'
-                className='block mb-2 text-lg font-medium'
-              >
-                Full Description
-              </label>
-              <div className='text-blue-700'>
-                In order to change Paragraph Write
-                <span className='font-bold text-xl'>" || "</span>
+          {greekFormShow ? (
+            <>
+              <motion.div>
+                <label
+                  htmlFor='Title'
+                  className='block mb-2 text-lg font-medium text-white-900'
+                >
+                  Title
+                </label>
+                <input
+                  ref={titleRef}
+                  type='text'
+                  id='Title'
+                  className='bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white'
+                  placeholder='John'
+                  required
+                />
+              </motion.div>
+              <motion.div>
+                <label
+                  htmlFor='shortDesc'
+                  className='block mb-2 text-lg font-medium text-white-900'
+                >
+                  Short Description
+                </label>
+                <input
+                  ref={shortDescRef}
+                  type='text'
+                  id='shortDesc'
+                  className='bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white'
+                  placeholder='Doe'
+                  required
+                />
+              </motion.div>
+              <motion.section className='col-span-2'>
+                <div className='lg:flex gap-4'>
+                  <label
+                    htmlFor='fullDesc'
+                    className='block mb-2 text-lg font-medium'
+                  >
+                    Full Description
+                  </label>
+                  <div className='text-blue-700'>
+                    In order to change Paragraph Write
+                    <span className='font-bold text-xl'>" || "</span>
+                  </div>
+                </div>
+                <textarea
+                  ref={fullDescRef}
+                  id='fullDesc'
+                  rows={4}
+                  className='block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 min-h-14'
+                  placeholder='Write your article`s Section'
+                  required={isFullDescRequired}
+                />
+              </motion.section>
+              <motion.div className='col-span-2'>
+                <label
+                  htmlFor='aTag'
+                  className='block mb-2 text-lg font-medium text-white-900'
+                >
+                  The Url that you want to Link instead of writing your article*
+                </label>
+                <input
+                  ref={aTagRef}
+                  type='text'
+                  id='aTag'
+                  className='bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white'
+                  placeholder='https://theuselessweb.com/'
+                />
+              </motion.div>
+            </>
+          ) : (
+            <>
+              <motion.div>
+                <label
+                  htmlFor='Title'
+                  className='block mb-2 text-lg font-medium text-white-900'
+                >
+                  GRTitle
+                </label>
+                <input
+                  ref={titleRef}
+                  type='text'
+                  id='Title'
+                  className='bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white'
+                  placeholder='John'
+                  required
+                />
+              </motion.div>
+              <div>
+                <label
+                  htmlFor='shortDesc'
+                  className='block mb-2 text-lg font-medium text-white-900'
+                >
+                  Short Description
+                </label>
+                <input
+                  ref={shortDescRef}
+                  type='text'
+                  id='shortDesc'
+                  className='bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white'
+                  placeholder='Doe'
+                  required
+                />
               </div>
-            </div>
-            <textarea
-              ref={fullDescRef}
-              id='fullDesc'
-              rows={4}
-              className='block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 min-h-14'
-              placeholder='Write your article`s Section'
-              required={isFullDescRequired}
-            />
-          </section>
-          <div className='col-span-2'>
-            <label
-              htmlFor='aTag'
-              className='block mb-2 text-lg font-medium text-white-900'
-            >
-              The Url that you want to Link instead of writing your article*
-            </label>
-            <input
-              ref={aTagRef}
-              type='text'
-              id='aTag'
-              className='bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white'
-              placeholder='https://theuselessweb.com/'
-            />
-          </div>
+              <section className='col-span-2'>
+                <div className='lg:flex gap-4'>
+                  <label
+                    htmlFor='fullDesc'
+                    className='block mb-2 text-lg font-medium'
+                  >
+                    Full Description
+                  </label>
+                  <div className='text-blue-700'>
+                    In order to change Paragraph Write
+                    <span className='font-bold text-xl'>" || "</span>
+                  </div>
+                </div>
+                <textarea
+                  ref={fullDescRef}
+                  id='fullDesc'
+                  rows={4}
+                  className='block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 min-h-14'
+                  placeholder='Write your article`s Section'
+                  required={isFullDescRequired}
+                />
+              </section>
+              <div className='col-span-2'>
+                <label
+                  htmlFor='aTag'
+                  className='block mb-2 text-lg font-medium text-white-900'
+                >
+                  The Url that you want to Link instead of writing your article*
+                </label>
+                <input
+                  ref={aTagRef}
+                  type='text'
+                  id='aTag'
+                  className='bg-white-50 border border-white-300 text-white-900 text-lg rounded-lg block w-full  p-2.5 white'
+                  placeholder='https://theuselessweb.com/'
+                />
+              </div>
+            </>
+          )}
           {/* Img drag n' drop */}
           <section className='w-fit'>
             <div className='bg-white-default border border-white-300 text-black-250 text-lg rounded-lg block w-full  p-2.5 white'>
@@ -176,7 +254,8 @@ export default function ArticlesUploadForm() {
         <div className='lg:flex gap-4 justify-between'>
           <div className='flex gap-3 items-center pb-3'>
             <button
-              type='submit'
+              type={greekFormShow ? 'submit' : 'button'}
+              onClick={() => setGreekFormShow(!greekFormShow)}
               className=' bg-primary hover:bg-primary-500 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center'
             >
               Submit
