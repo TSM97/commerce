80adfe59dce4132bd2f38415005261d0200d3e14
GiannisTsamadias <127104855+GiannisTsamadias@users.noreply.github.com>
2025-01-19 23:15:31 +0200

    Implement( CustomInput component )

---
+++ src/Components/Common/Input/CustomInput.tsx
@@ -0,0 +1,43 @@
+import { ChangeEventHandler } from 'react';
+
+type CustomInputType = {
+  OnChange: ChangeEventHandler<HTMLInputElement>;
+  Name: string;
+  Label: string;
+  Id: string;
+  Type: 'text' | 'password' | 'email' | 'number' | 'checkbox' | 'radio';
+  value: any;
+  ClassName?: string;
+};
+
+export default function CustomInput({
+  OnChange,
+  Name,
+  Label,
+  Id,
+  Type,
+  value,
+  ClassName,
+}: CustomInputType) {
+  return (
+    <div className={`relative ${ClassName}`}>
+      <input
+        type={Type}
+        name={Name}
+        id={Id}
+        onChange={OnChange}
+        placeholder=' '
+        className='peer shadow bg-transparent appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-honey'
+        required
+      />
+      <label
+        htmlFor={Id}
+        className={`absolute left-3 top-2 bg-white px-1 text-gray-500 text-sm font-bold transition-all peer-placeholder-shown:cursor-text peer-focus:cursor-default peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-base ${
+          value != '' ? ' top-[-10px]' : 'peer-focus:top-[-10px] '
+        } peer-focus:text-honey peer-focus:text-sm`}
+      >
+        {Label}
+      </label>
+    </div>
+  );
+}
--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useContactFormStore } from "../../stores/useContactFormStore";
+import CustomInput from '../Common/Input/CustomInput';
+import { useContactFormStore } from '../../stores/useContactFormStore';
 
 export default function Contact() {
   const { formData, setField, resetForm } = useContactFormStore();
@@ -7,59 +7,35 @@ export default function Contact() {
   console.log(formData.name);
   return (
     <section
-      id="Contact"
-      className="min-h-[90vh] flex justify-center items-start"
+      id='Contact'
+      className='min-h-[90vh] flex justify-center items-start'
     >
-      <section className="w-3/4 relative border-4 border-dashed border-primary-100 items-center flex">
-        <div className="w-1/2 p-7 border-r-4 border-dashed border-primary-100">
+      <section className='w-3/4 relative border-4 border-dashed border-primary-100 items-center flex'>
+        <div className='w-1/2 p-7 border-r-4 border-dashed border-primary-100'>
           <form>
-            <h2 className="font-extrabold text-[1.3vw]">CONTACT US</h2>
-            <div className="relative mb-4">
-              <input
-                type="text"
-                name="name"
-                id="name"
-                onChange={(e) => setField(e.target.name, e.target.value)}
-                placeholder=" "
-                className="peer mb-5 shadow bg-transparent appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-honey"
-                required
-              />
-              <label
-                htmlFor="name"
-                className={`absolute left-3 top-2 peer-placeholder-shown:top-2.5 bg-white px-1 text-gray-500 text-sm font-bold transition-all  peer-placeholder-shown:text-gray-700 peer-placeholder-shown:text-base ${
-                  formData?.name != ""
-                    ? " top-[-10px]"
-                    : "peer-focus:top-[-10px] "
-                } peer-focus:text-honey peer-focus:text-sm`}
-              >
-                Name
-              </label>
-            </div>
-            <div className="relative mb-2">
-              <input
-                type="text"
-                name="email"
-                id="email"
-                onChange={(e) => setField(e.target.name, e.target.value)}
-                placeholder=" "
-                className="peer shadow bg-transparent appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-honey"
-                required
-              />
-              <label
-                htmlFor="email"
-                className={`absolute left-3 top-2 peer-placeholder-shown:top-2.5 bg-white px-1 text-gray-500 text-sm font-bold transition-all  peer-placeholder-shown:text-gray-700 peer-placeholder-shown:text-base ${
-                  formData?.email != ""
-                    ? " top-[-10px]"
-                    : "peer-focus:top-[-10px] "
-                } peer-focus:text-honey peer-focus:text-sm`}
-              >
-                Εmail
-              </label>
-            </div>
+            <h2 className='font-extrabold text-[1.3vw]'>CONTACT US</h2>
+            <CustomInput
+              ClassName='mb-4'
+              Name='name'
+              Label='Name'
+              Type='text'
+              Id='name'
+              OnChange={(e) => setField(e.target.name, e.target.value)}
+              value={formData?.name}
+            />
+            <CustomInput
+              ClassName='mb-2'
+              Name='email'
+              Label='Email'
+              Type='email'
+              Id='email'
+              OnChange={(e) => setField(e.target.name, e.target.value)}
+              value={formData?.email}
+            />
           </form>
         </div>
-        <div className="w-1/2 p-7"></div>
-        <div className="text-primary-100 px-4 text-3xl font-extrabold -bottom-[18px] transform left-1/4 translate-x-[-50%]  absolute bg-white">
+        <div className='w-1/2 p-7'></div>
+        <div className='text-primary-100 px-4 text-3xl font-extrabold -bottom-[18px] transform left-1/4 translate-x-[-50%]  absolute bg-white'>
           ATHEENIAN BEES
         </div>
       </section>
