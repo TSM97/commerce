db76db705d3c446ac3b156d877f38f540c93eef6
TSM97 <gtsamadias@hotmail.com>
2025-01-31 19:49:00 +0200

    Add(Styling to admin sign in form )

--- src/Auth/AdminLogin/AdminLogin.tsx
+++ src/Auth/AdminLogin/AdminLogin.tsx
@@ -2,41 +2,84 @@ import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebaseConfig";
 import { useNavigate } from "react-router-dom";
+import Spinner from "../../Components/Common/Spinner/Spinner";
+import CustomButton from "../../Components/Common/CustomButton";
+import CustomInput from "../../Components/Common/Input/CustomInput";
 
 const AdminLogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = async (e: any) => {
     e.preventDefault();
     try {
+      setIsLoading(true);
       await signInWithEmailAndPassword(auth, email, password);
-      console.log("Login successful!");
+      setIsLoading(false);
       navigate("/admin");
     } catch (err) {
       setError("Invalid credentials");
+      setIsLoading(false);
     }
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-      <button type="submit">Login</button>
-      {error && <p>{error}</p>}
-    </form>
+    <section className="min-h-screen flex justify-center items-center">
+      <div className="w-1/4 p-8 min-h-[450px] border-4 border-dashed border-primary-100 text-lg">
+        {" "}
+        <form onSubmit={handleLogin}>
+          <h2 className="font-extrabold mb-4 text-[1.3vw] text-center">
+            Sign In
+          </h2>
+          <CustomInput
+            ClassName="mb-4"
+            Type="email"
+            Name="email"
+            Id="email"
+            OnChange={(e) => setEmail(e.target.value)}
+            Label="email"
+            value={email}
+          />
+          <CustomInput
+            ClassName="mb-4"
+            Name="password"
+            Label="password"
+            Type="password"
+            Id="password"
+            OnChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <div className="flex flex-col items-center">
+            <CustomButton
+              type="submit"
+              className={`w-full ${
+                isLoading ? "scale-0 opacity-0" : "scale-100 opacity-100"
+              } transition-transform duration-500`}
+              disabled={isLoading}
+            >
+              {isLoading ? "Validating..." : "Continue"}
+            </CustomButton>
+            {error && <p>{error}</p>}
+            <div
+              className={`scale-0 ${
+                isLoading ? "scale-100 opacity-100" : "scale-0 opacity-0"
+              } transition-transform duration-500 text-center`}
+            >
+              <div className="mt-4">
+                <p>
+                  Please wait, we are processing your request. This might take a
+                  few seconds. Don't close or refresh the page.
+                </p>
+                <Spinner ClassName="absolute top-0 left-1/2" />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
