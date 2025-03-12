4ba84d5903737a6982381801d0c820c54133cf9e
GiannisTsamadias <127104855+GiannisTsamadias@users.noreply.github.com>
2025-03-12 17:38:20 +0200

    Init LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
-import { useEffect } from "react";
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 // import Lenis from 'lenis';
 
-import NavBar from "./Components/NavBar";
-import Admin from "./AdminPage/Admin";
-import { Hero, Intro } from "./Components/Hero";
-import News from "./Components/News/News";
-import { ArticlePage } from "./Components/ArticlePage";
-import Products from "./Components/Products";
-import About from "./Components/About";
-import "./App.css";
-import { useArticlesStore } from "./stores/useArticlesStore";
-import { useProductsStore } from "./stores/useProductsStore";
-import Contact from "./Components/Contact";
-import ProtectedRoute from "./Auth/ProtectedRoute";
-import AuthRedirect from "./Auth/AuthRedirect";
+import NavBar from './Components/NavBar';
+import Admin from './AdminPage/Admin';
+import { Hero, Intro } from './Components/Hero';
+import News from './Components/News/News';
+import { ArticlePage } from './Components/ArticlePage';
+import Products from './Components/Products';
+import About from './Components/About';
+import './App.css';
+import { useArticlesStore } from './stores/useArticlesStore';
+import { useProductsStore } from './stores/useProductsStore';
+import Contact from './Components/Contact';
+import ProtectedRoute from './Auth/ProtectedRoute';
+import AuthRedirect from './Auth/AuthRedirect';
+
+const FETCH_TIMEOUT = 3 * 60 * 1000; // 3 minutes
 
 export default function App() {
   const location = useLocation(); // Get the current location
@@ -24,22 +26,29 @@ export default function App() {
   const { fetchProducts, products } = useProductsStore();
 
   useEffect(() => {
-    fetchArticles(); // Fetch articles on component mount
-    fetchProducts();
+    const lastFetchTimestamp =
+      Number(localStorage.getItem('lastFetchTimestamp')) || 0;
+    const now = Date.now();
+    console.log(lastFetchTimestamp, now);
+    if (now - lastFetchTimestamp > FETCH_TIMEOUT) {
+      fetchArticles(); // Fetch articles on component mount
+      fetchProducts();
+      localStorage.setItem('lastFetchTimestamp', now.toString());
+    }
   }, [fetchArticles, fetchProducts]);
 
   console.log(products);
 
-  const isAdminPage = location.pathname.includes("/admin");
+  const isAdminPage = location.pathname.includes('/admin');
 
   return (
-    <section id="main">
-      <section className="flex justify-center w-100dvw">
+    <section id='main'>
+      <section className='flex justify-center w-100dvw'>
         {!isAdminPage && <NavBar />}
       </section>
       <Routes>
         <Route
-          path="/"
+          path='/'
           element={
             <>
               <Intro />
@@ -52,15 +61,15 @@ export default function App() {
           }
         />
         <Route
-          path="/admin"
+          path='/admin'
           element={
             <ProtectedRoute>
               <Admin />
             </ProtectedRoute>
           }
         />
-        <Route path="/admin/auth" element={<AuthRedirect />} />
-        <Route path="/article/:id" element={<ArticlePage />} />
+        <Route path='/admin/auth' element={<AuthRedirect />} />
+        <Route path='/article/:id' element={<ArticlePage />} />
       </Routes>
     </section>
   );
