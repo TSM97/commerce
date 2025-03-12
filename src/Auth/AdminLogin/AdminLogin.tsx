b0fe3683a95264ff2141eee4cd8c3a598cd3c60b
GiannisTsamadias <127104855+GiannisTsamadias@users.noreply.github.com>
2025-03-12 17:37:16 +0200

    update(NodeModules) + minor fixes

--- src/Auth/AdminLogin/AdminLogin.tsx
+++ src/Auth/AdminLogin/AdminLogin.tsx
@@ -1,80 +1,81 @@
-import { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../firebaseConfig";
-import { useNavigate } from "react-router-dom";
-import Spinner from "../../Components/Common/Spinner/Spinner";
-import CustomButton from "../../Components/Common/CustomButton";
-import CustomInput from "../../Components/Common/Input/CustomInput";
-import { t } from "i18next";
+import { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
+import { useNavigate } from 'react-router-dom';
+import Spinner from '../../Components/Common/Spinner/Spinner';
+import CustomButton from '../../Components/Common/CustomButton';
+import CustomInput from '../../Components/Common/Input/CustomInput';
+import { t } from 'i18next';
 
 const AdminLogin = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = async (e: any) => {
+    setError('');
     e.preventDefault();
     try {
       setIsLoading(true);
       await signInWithEmailAndPassword(auth, email, password);
       setIsLoading(false);
-      navigate("/admin");
+      navigate('/admin');
     } catch (err) {
-      setError("Invalid credentials");
+      setError('Invalid credentials');
       setIsLoading(false);
     }
   };
 
   return (
-    <section className="min-h-screen flex justify-center items-center">
-      <div className="w-5/6 lg:w-1/4 p-8 md:min-h-[450px] border-4 border-dashed border-primary-100 text-lg">
+    <section className='min-h-screen flex justify-center items-center'>
+      <div className='w-5/6 lg:w-1/4 p-8 md:min-h-[450px] border-4 border-dashed border-primary-100 text-lg'>
         <form onSubmit={handleLogin}>
-          <h2 className="font-extrabold mb-4 text-2xl lg:text-[1.3vw] text-center">
+          <h2 className='font-extrabold mb-4 text-2xl lg:text-[1.3vw] text-center'>
             Sign In
           </h2>
           <CustomInput
-            ClassName="mb-4"
-            Type="email"
-            Name="email"
-            Id="email"
+            ClassName='mb-4'
+            Type='email'
+            Name='email'
+            Id='email'
             OnChange={(e) => setEmail(e.target.value)}
-            Label="Email"
+            Label='Email'
             value={email}
           />
           <CustomInput
-            ClassName="mb-4"
-            Name="password"
-            Label={t("password")}
-            Type="password"
-            Id="password"
+            ClassName='mb-4'
+            Name='password'
+            Label={t('password')}
+            Type='password'
+            Id='password'
             OnChange={(e) => setPassword(e.target.value)}
             value={password}
           />
-          <div className="flex flex-col items-center">
+          <div className='flex flex-col items-center'>
             <CustomButton
-              type="submit"
+              type='submit'
               className={`w-full ${
-                isLoading ? "scale-0 opacity-0" : "scale-100 opacity-100"
+                isLoading ? 'scale-0 opacity-0' : 'scale-100 opacity-100'
               } transition-transform duration-500`}
               disabled={isLoading}
             >
-              {isLoading ? "Validating..." : "Continue"}
+              {isLoading ? 'Validating...' : 'Continue'}
             </CustomButton>
             {error && <p>{error}</p>}
             <div
               className={`scale-0 ${
-                isLoading ? "scale-100 opacity-100" : "scale-0 opacity-0"
+                isLoading ? 'scale-100 opacity-100' : 'scale-0 opacity-0'
               } transition-transform duration-500 text-center`}
             >
               {isLoading && (
-                <div className="mt-4">
+                <div className='mt-4'>
                   <p>
                     Please wait, we are processing your request. This might take
                     a few seconds. Don't close or refresh the page.
                   </p>
-                  <Spinner ClassName="absolute top-0 left-1/2" />
+                  <Spinner ClassName='absolute top-0 left-1/2' />
                 </div>
               )}
             </div>
