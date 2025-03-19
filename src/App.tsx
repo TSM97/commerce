84b3c23c1f19baa94874fc8fa773322b989396b5
GiannisTsamadias <127104855+GiannisTsamadias@users.noreply.github.com>
2025-03-19 17:53:47 +0200

    Initialize( FOOTER )

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
-import { useEffect } from "react";
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import './App.css';
 // import Lenis from 'lenis';
 
-import NavBar from "./Components/NavBar";
-import Admin from "./AdminPage/Admin";
-import { Hero, Intro } from "./Components/Hero";
-import News from "./Components/News/News";
-import { ArticlePage } from "./Components/ArticlePage";
-import Products from "./Components/Products";
-import About from "./Components/About";
-import "./App.css";
-import { useArticlesStore } from "./stores/useArticlesStore";
-import { useProductsStore } from "./stores/useProductsStore";
-import Contact from "./Components/Contact";
-import ProtectedRoute from "./Auth/ProtectedRoute";
-import AuthRedirect from "./Auth/AuthRedirect";
+import NavBar from './Components/NavBar';
+import Admin from './AdminPage/Admin';
+import { Footer } from './Components/Footer';
+import { Hero, Intro } from './Components/Hero';
+import News from './Components/News/News';
+import { ArticlePage } from './Components/ArticlePage';
+import Products from './Components/Products';
+import About from './Components/About';
+import { useArticlesStore } from './stores/useArticlesStore';
+import { useProductsStore } from './stores/useProductsStore';
+import Contact from './Components/Contact';
+import ProtectedRoute from './Auth/ProtectedRoute';
+import AuthRedirect from './Auth/AuthRedirect';
 
 export default function App() {
   const location = useLocation(); // Get the current location
@@ -30,16 +31,16 @@ export default function App() {
 
   console.log(products);
 
-  const isAdminPage = location.pathname.includes("/admin");
+  const isAdminPage = location.pathname.includes('/admin');
 
   return (
-    <section id="main">
-      <section className="flex justify-center w-100dvw">
+    <section id='main'>
+      <section className='flex justify-center w-100dvw'>
         {!isAdminPage && <NavBar />}
       </section>
       <Routes>
         <Route
-          path="/"
+          path='/'
           element={
             <>
               <Intro />
@@ -48,19 +49,20 @@ export default function App() {
               <News />
               <About />
               <Contact />
+              <Footer />
             </>
           }
         />
         <Route
-          path="/admin"
+          path='/admin'
           element={
             <ProtectedRoute>
               <Admin />
             </ProtectedRoute>
           }
         />
-        <Route path="/admin/auth" element={<AuthRedirect />} />
-        <Route path="/article/:id" element={<ArticlePage />} />
+        <Route path='/admin/auth' element={<AuthRedirect />} />
+        <Route path='/article/:id' element={<ArticlePage />} />
       </Routes>
     </section>
   );
---
+++ src/Components/Footer/FooterDetails.tsx
@@ -0,0 +1,11 @@
+export default function FooterDetails() {
+  return (
+    <section className='flex flex-col h-full justify-between'>
+      <div>as</div>
+      <div>as</div>
+      <div className=' text-5xl xl:text-[6dvw] tracking-wide font-extrabold'>
+        ATHENIAN BEES
+      </div>
+    </section>
+  );
+}
